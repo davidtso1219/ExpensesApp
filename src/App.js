27e8939bca367c92b75e08f371fffac6baf241f6
david tso <davidtso1219@gmail.com>
2022-09-17 16:46:04 +0800

    Added NewExpense component.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -9,6 +10,7 @@ const App = () => {
 
   return (
     <div className="App">
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+
+import './ExpenseForm.css';
+
+const ExpenseForm = (props) => {
+
+  const [title, setTitle] = useState('');
+  const [amount, setAmount] = useState('');
+  const [date, setDate] = useState('');
+
+  const clearForm = () => {
+    setTitle('');
+    setAmount('');
+    setDate('');
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const newExpenseData = {
+      title,
+      amount,
+      date
+    }
+
+    clearForm();
+    console.log(newExpenseData);
+    return newExpenseData;
+  };
+
+  const titleChangeHandler = e => {
+    e.preventDefault();
+    setTitle(e.target.value);
+  };
+
+  const amountChangeHandler = e => {
+    e.preventDefault();
+    setAmount(e.target.value);
+  };
+
+  const dateChangeHandler = e => {
+    e.preventDefault();
+    setDate(e.target.value);
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className='new-expense__controls'>
+        <div className='new-expense__control'>
+          <label>Title</label>
+          <input type='text' value={title} onChange={titleChangeHandler}/>
+        </div>
+        <div className='new-expense__control'>
+          <label>Amount</label>
+          <input type='number' value={amount} onChange={amountChangeHandler} min='0.01' step='0.01'/>
+        </div>
+        <div className='new-expense__control'>
+          <label>Date</label>
+          <input type='date' value={date} onChange={dateChangeHandler} min='2022-01-01' max='2024-12-31'/>
+        </div>
+      </div>
+      <div className='new-expense__actions'>
+        <button type='submit'>Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
